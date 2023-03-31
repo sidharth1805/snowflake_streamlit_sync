e2fd2f6a7a44678f0aa674e96a14edd193268ecb
Sidharth Ramalingam <sidharth.ramalingam@gmail.com>
2023-03-31 13:10:16 +0530

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from TRIPS;")
 
 # Print results.
 for row in rows:
