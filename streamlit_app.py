34460460542830767597feac0d89084ee04ccbb3
Sidharth Ramalingam <sidharth.ramalingam@gmail.com>
2023-03-31 15:18:51 +0530

    bar_chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,16 @@ def run_query(query):
         return cur.fetchall()
 
 rows = run_query("SELECT * from TRIPS limit 10;")
+rows_2 = run_query("""select START_STATION_ID,start_station_name,sum(TRIPDURATION) as Total_Hours
+from TRIPS
+group by START_STATION_ID,start_station_name
+order by sum(TRIPDURATION) DESC
+Limit 10;""")
 st.write("hello")
 #st.write(rows)
 columns =["TRIPDURATION","STARTTIME",	"STOPTIME"	,"START_STATION_ID"	,"START_STATION_NAME"	,"START_STATION_LATITUDE"	,"START_STATION_LONGITUDE"	,"END_STATION_ID"	,"END_STATION_NAME"	,"END_STATION_LATITUDE"	,"END_STATION_LONGITUDE","BIKEID"	,"MEMBERSHIP_TYPE"	,"USERTYPE"	,"BIRTH_YEAR"	,"GENDER"]
+columns_2 = ["START_STATION_ID", "START_STATION_NAME", "TOTAL_HOURS"]
 df = pd.DataFrame(rows, columns=columns)
+df_2 = pd.DataFrame(rows_2, columns=columns_2)
 st.write(df)
+st.bar_chart(data=df_2, x="START_STATION_NAME", y="TOTAL_HOURS", width=0, height=0, use_container_width=True)
